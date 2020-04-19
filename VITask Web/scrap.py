f4b5453dd726933f815b3230c789cfb6f7590523
Apratim Shukla <apratimshukla6@gmail.com>
2020-04-19 21:29:46 +0530

    Updated scrap.py for reference

--- VITask Web/scrap.py
+++ VITask Web/scrap.py
@@ -10,32 +10,33 @@ import os
 import firebase_admin
 from firebase_admin import db
 
-
 with open("C:\\Users\\aprat\\Documents\\GitHub\\VITask\\VITask Web\\test.html", encoding="utf8") as fp:
     soup = BeautifulSoup(fp, 'lxml')
-    code_soup = soup.findAll('tbody')
+    code_soup = soup.findAll("table", {"class": "customTable-level1"})
     code_soup2 = soup.findAll("tr", {"class": "tableContent"})
+    
     courses = []
     temp = []
-    for i in code_soup2:
-        temp = i.findAll('td')
-        if(len(temp)==9):
-            courses.append(temp[3].getText()+" "+temp[4].getText())
-
-    code_soup = code_soup[1:len(code_soup)]
-    courseMarks = []
+    for i in range(0,len(code_soup2),2):
+        temp = code_soup2[i].findAll("td")
+        courses.append(temp[3].getText()+" "+temp[4].getText())
+        temp = []
+        
+    temp = []
+    hold = []
+    temp_dict = {}
+    hold_array = []
     for i in code_soup:
-        courseMarks.append(i.findAll('tr'))
-
-    k = []
-    m = 0
-    tempDict = {}
-    marksDict = {} 
-    for i in range (0,len(courseMarks)):
-        for j in range(1, len(courseMarks[i])):
-            k = courseMarks[i][j].findAll('td')
-            tempDict[k[1].getText()] = k[5].getText() 
-        marksDict[courses[m]] =  tempDict
-        m = m+1
-        tempDict = {}
-    print(marksDict)
+        temp = i.findAll("tr", {"class": "tableContent-level1"})
+        for j in temp:
+            hold = j.findAll("td")
+            temp_dict[hold[1].getText()] = hold[5].getText()
+        hold_array.append(temp_dict)
+        temp_dict = {}
+        
+    marksDict = {}
+    for i in range(0,len(courses)):
+        marksDict[courses[i]] = hold_array[i]
+        print(marksDict)
+        
+        
